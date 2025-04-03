c3f292dd631f16974054c0da362bf19d39681b0f
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-04-03 22:49:45 +0000

    Removing egress rule since it is not needed

--- terragrunt/aws/ecs/security_group.tf
+++ terragrunt/aws/ecs/security_group.tf
@@ -38,16 +38,6 @@ resource "aws_security_group_rule" "ecs_egress_all" {
 # Traffic to DocumentDB should only come from ECS
 ###
 
-resource "aws_security_group_rule" "ecs_egress_database" {
-  description              = "Allow ECS to talk to the DocumentDB cluster"
-  type                     = "egress"
-  from_port                = 27017
-  to_port                  = 27017
-  protocol                 = "tcp"
-  source_security_group_id = var.aws_docdb_security_group_id
-  security_group_id        = aws_security_group.ecs_tasks.id
-}
-
 resource "aws_security_group_rule" "database_ingress_ecs" {
   description              = "Allow DocumentDB cluster to receive requests from ECS"
   type                     = "ingress"
