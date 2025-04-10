9b6af2d1a0c6b13f9766c888c75c7aa756322e7f
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-04-10 08:13:38 -0400

    fix: change value from prod to production

--- terragrunt/aws/iam/oidc.tf
+++ terragrunt/aws/iam/oidc.tf
@@ -7,9 +7,9 @@ locals {
 # This role can be assumed by GitHub workflows based on the claim
 #
 module "github_workflow_roles" {
-  count = var.env == "prod" ? 1 : 0
+  count = var.env == "production" ? 1 : 0
 
-  source            = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v10.3.2"
+  source            = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v10.4.1"
   billing_tag_value = var.billing_code
 
   roles = [
@@ -25,7 +25,7 @@ module "github_workflow_roles" {
 # IAM policy allowing ECR deploy and ECS update
 #
 resource "aws_iam_policy" "ecr_deploy_policy" {
-  count = var.env == "prod" ? 1 : 0
+  count = var.env == "production" ? 1 : 0
 
   name        = "ai-answers-ecr-deploy-policy"
   description = "Policy for GitHub Actions to deploy to ECR and update ECS"
@@ -63,7 +63,7 @@ resource "aws_iam_policy" "ecr_deploy_policy" {
 # Attach the IAM policy to the OIDC role
 #
 resource "aws_iam_role_policy_attachment" "ai_answers_release" {
-  count = var.env == "prod" ? 1 : 0
+  count = var.env == "production" ? 1 : 0
 
   role       = local.ai_answers_release
   policy_arn = aws_iam_policy.ecr_deploy_policy[0].arn
