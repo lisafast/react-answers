2255cc9649f8294e33cc3b4cc93b1d193d16a85c
Lisa Fast <lisa@vation.ca>
2025-05-01 17:40:38 -0400

    still catching dollar amounts

--- src/services/RedactionService.js
+++ src/services/RedactionService.js
@@ -329,7 +329,7 @@ class RedactionService {
         description: 'URLs'
       },
       {
-        pattern: /(?<!\$)(?!\d{4}\b)\b\d{5,}\b/g,
+        pattern: /(?<![\$\d])\b\d{5,}\b/g,
         description: 'Long number sequences'
       },
       {
